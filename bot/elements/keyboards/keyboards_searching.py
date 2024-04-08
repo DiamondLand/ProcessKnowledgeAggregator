6a747d648b6571cf10ede0f324203aad1e5fad34
Diamond Land <81316675+DiamondLand@users.noreply.github.com>
2024-04-08 20:04:31 +0300

    back btn

--- bot/elements/keyboards/keyboards_searching.py
+++ bot/elements/keyboards/keyboards_searching.py
@@ -1,14 +1,16 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
-from .text_on_kb import (next_question, vote_question, answer_question, edit_my_question, next_my_question, answer_my_question)
+from .text_on_kb import (next_question, vote_question, answer_question, edit_my_question,
+                         view_answers_my_question, view_answers_question, next_my_question, answer_my_question, back)
 
 
 # --- Панель просмотра всех вопросов ---
 def all_questions_kb() -> ReplyKeyboardMarkup:
     kb = [
-        [KeyboardButton(text=next_question)], 
+        [KeyboardButton(text=vote_question), KeyboardButton(text=next_question)],
+        [KeyboardButton(text=view_answers_question)],
         [KeyboardButton(text=answer_question)],
-        [KeyboardButton(text=vote_question)]
+        [KeyboardButton(text=back)]
     ]
     return ReplyKeyboardMarkup(
         keyboard=kb,
@@ -20,9 +22,10 @@ def all_questions_kb() -> ReplyKeyboardMarkup:
 # --- Панель просмотра собственных вопросов ---
 def my_questions_kb() -> ReplyKeyboardMarkup:
     kb = [
-        [KeyboardButton(text=edit_my_question)],
-        [KeyboardButton(text=next_my_question)], 
-        [KeyboardButton(text=answer_my_question)]
+        [KeyboardButton(text=edit_my_question), KeyboardButton(text=next_my_question)],
+        [KeyboardButton(text=answer_my_question)],
+        [KeyboardButton(text=view_answers_my_question)],
+        [KeyboardButton(text=back)]
     ]
     return ReplyKeyboardMarkup(
         keyboard=kb,
--- bot/events/profile_events.py
+++ bot/events/profile_events.py
@@ -1,12 +1,11 @@
-import httpx
-
 from aiogram import Router, F
-from aiogram.types import Message, CallbackQuery, ReplyKeyboardRemove
+from aiogram.types import Message, ReplyKeyboardRemove
 from aiogram.fsm.context import FSMContext
-from aiogram.exceptions import TelegramBadRequest
 
 from elements.keyboards.keyboards_profile import profile_kb
-from elements.keyboards.keyboards_utilits import cancel_button
+from elements.keyboards.text_on_kb import cancel_button, back
+
+from handlers.commands.commands_handler import cmd_start
 
 from .states_group import Captcha
 
@@ -31,6 +30,18 @@ async def cancel_handler(message: Message, state: FSMContext):
         text="<b>Действие прервано!</b>\n\nДля возвращения в главную панель воспользуйтесь /profile!",
         reply_markup=ReplyKeyboardRemove()
     )
+
+
+# --- Возвращение в панель аккаунтав --- #
+@router.message(F.text == back)
+async def back_handler(message: Message, state: FSMContext):
+    # Если стадия существует, выходим из неё
+    current_state = await state.get_state()
+    if current_state is not None:
+        await state.clear()
+
+    await cmd_start(message=message, state=state)
+
 
 # --- Обработчик ввода каптчи --- #
 @router.message(Captcha.captcha_input)
